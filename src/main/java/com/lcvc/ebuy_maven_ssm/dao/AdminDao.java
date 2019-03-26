3f97d151483ce8b8a2b9acfd145c50ae1ea00fcf
Fdxabc <2240543353@qq.com>
2019-03-26 12:10:06 +0800

    第二次上传，修改完善

---
+++ src/main/java/com/lcvc/ebuy_maven_ssm/dao/AdminDao.java
@@ -0,0 +1,78 @@
+package com.lcvc.ebuy_maven_ssm.dao;
+
+import com.lcvc.ebuy_maven_ssm.model.Admin;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class AdminDao {
+	/**
+	 * 根据账户名和密码去数据库查询，进行登录判断
+	 * @param username 账户名
+	 * @param password 密码
+	 * @return 不为null表示登录成功,null表示登录失败
+	 */
+	public Admin login(String username,String password){
+		Admin admin=null;//null表示登录状态
+		Connection conn=DBHelper.getConnection();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try{
+			String sql="select * from admin where username=? and password=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, username);
+			pstmt.setString(2, password);
+
+			rs=pstmt.executeQuery();
+			if(rs.next()){//如果能够从数据库找到记录
+				admin=new Admin();
+				admin.setId(rs.getInt("id"));
+				admin.setUsername(rs.getString("username"));
+				admin.setPassword(rs.getString("password"));
+				admin.setName(rs.getString("name"));
+			}
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			DBHelper.closeConn(conn,pstmt,rs);
+		}
+		return admin;
+	}
+	/*public Admin getAdmin(String username){
+		Admin admin=null;//null表示找不到该账户
+		Connection conn=DBHelper.getConnection();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try{
+			String sql="select * from admin where username=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, username);
+			rs=pstmt.executeQuery();
+			if(rs.next()){//如果能够从数据库找到记录
+				admin=new Admin();
+				admin.setId(rs.getInt("id"));
+				admin.setUsername(rs.getString("username"));
+				admin.setPassword(rs.getString("password"));
+				admin.setName(rs.getString("name"));
+			}
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			DBHelper.closeConn(conn,pstmt,rs);
+		}
+		return admin;
+
+	}*/
+
+	public static void main(String[] args) {
+		AdminDao adminDao=new AdminDao();
+	//	System.out.println(adminDao.login("admin", "123"));
+	//	System.out.println(adminDao.login("user", "123"));
+	//	System.out.println(adminDao.login("user", "123456"));
+
+
+	}
+}
---
+++ src/main/java/com/lcvc/ebuy_maven_ssm/model/Admin.java
@@ -0,0 +1,40 @@
+package com.lcvc.ebuy_maven_ssm.model;
+
+public class Admin {
+    private Integer id;//编号
+    private  String username;//用户名
+    private  String password;//密码
+    private  String name;//姓名
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
